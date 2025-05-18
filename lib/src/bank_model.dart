cd708d9f19a63a2d703d80fbe1d4e8afb9eee346
Amir Ahadi <49079889+amirahadi@users.noreply.github.com>
2025-05-19 01:15:15 +0330

    list update

--- lib/src/bank_model.dart
+++ lib/src/bank_model.dart
@@ -1,29 +1,34 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class IranianBank {
   IranianBank({
     required this.bankName,
     required this.bankTitle,
     required this.bankLogoPath,
-    required this.mainColor,
-    required this.lighterColor,
-    required this.darkerColor,
     required this.iban,
     required this.cardRegex,
     required this.ibanRegex,
+    required this.prefix_card_regex,
+    required this.prefix_iban_regex,
+    required this.mainColor,
+    required this.lighterColor,
+    required this.darkerColor,
   });
 
-  factory IranianBank.fromJson(Map<dynamic, dynamic> json) {
+  factory IranianBank.fromJson(Map<String, dynamic> json) {
     return IranianBank(
       bankName: json['bank_name'],
       bankTitle: json['bank_title'],
       bankLogoPath: json['bank_logo'],
-      mainColor: _hexToColor(json['color']),
-      lighterColor: _hexToColor(json['lighter_color']),
-      darkerColor: _hexToColor(json['darker_color']),
       iban: json['iban'],
       cardRegex: json['card_regex'],
       ibanRegex: json['iban_regex'],
+      prefix_card_regex: json['prefix_card_regex'],
+      prefix_iban_regex: json['prefix_iban_regex'],
+      mainColor: _hexToColor(json['color']),
+      lighterColor: _hexToColor(json['lighter_color']),
+      darkerColor: _hexToColor(json['darker_color']),
     );
   }
 
@@ -31,13 +36,29 @@ class IranianBank {
   final String bankName;
   final String bankTitle;
   final String cardRegex;
-  final Color darkerColor;
   final String iban;
   final String ibanRegex;
-  final Color lighterColor;
+  final String prefix_card_regex;
+  final String prefix_iban_regex;
   final Color mainColor;
+  final Color lighterColor;
+  final Color darkerColor;
 
-  /// Change the color from #ffffff (example) to the Color object.
+  /// logo builder from svg path
+  SvgPicture logoBuilder({
+    double width = 40,
+    double height = 40,
+    BoxFit fit = BoxFit.contain,
+  }) {
+    return SvgPicture.asset(
+      bankLogoPath.replaceFirst('./', 'assets/'),
+      width: width,
+      height: height,
+      fit: fit,
+    );
+  }
+
+  /// change the color like #FFFFFF (example) to the Color object like 0xFFFFFFFF.
   static Color _hexToColor(String hex) {
     hex = hex.replaceAll('#', '');
     if (hex.length == 6) {
--- lib/src/bank_service.dart
+++ lib/src/bank_service.dart
@@ -6,7 +6,6 @@ import 'bank_model.dart';
 class IranianBankInfo {
   static final List<IranianBank> _banks = [];
 
-  /// لود کردن اطلاعات از فایل json داخل پکیج
   static Future<void> init() async {
     if (_banks.isNotEmpty) return;
 
@@ -17,7 +16,6 @@ class IranianBankInfo {
     final List<dynamic> jsonData = json.decode(data);
     _banks.clear();
     _banks.addAll(jsonData.map((e) {
-      // مسیر لوگو را به مسیر کامل داخل پکیج تبدیل می‌کنیم
       final logoPath =
           'packages/iranian_banks/${e['bank_logo'].replaceFirst('./', 'assets/')}';
 
@@ -28,14 +26,17 @@ class IranianBankInfo {
     }));
   }
 
-  /// پیدا کردن بانک بر اساس شماره کارت
+  static IranianBank? findByPrefixCardNumber(String cardNumber) {
+    for (var bank in _banks) {
+      final regex = RegExp(bank.prefix_card_regex);
+      if (regex.hasMatch(cardNumber)) {
+        return bank;
+      }
+    }
+    return null;
+  }
+
   static IranianBank? findByCardNumber(String cardNumber) {
-    // var reformCardNumber = cardNumber;
-    // if (reformCardNumber.length != 16) {
-    //   for (var i = reformCardNumber.length; i < 16; i++) {
-    //     reformCardNumber = reformCardNumber + '0';
-    //   }
-    // }
     for (var bank in _banks) {
       final regex = RegExp(bank.cardRegex);
       if (regex.hasMatch(cardNumber)) {
@@ -45,7 +46,16 @@ class IranianBankInfo {
     return null;
   }
 
-  /// پیدا کردن بانک بر اساس شماره شبا
+  static IranianBank? findByPrefixIBAN(String iban) {
+    for (var bank in _banks) {
+      final regex = RegExp(bank.prefix_iban_regex);
+      if (regex.hasMatch(iban)) {
+        return bank;
+      }
+    }
+    return null;
+  }
+
   static IranianBank? findByIBAN(String iban) {
     for (var bank in _banks) {
       final regex = RegExp(bank.ibanRegex);
@@ -56,6 +66,5 @@ class IranianBankInfo {
     return null;
   }
 
-  /// لیست کامل بانک‌ها
   static List<IranianBank> get allBanks => List.unmodifiable(_banks);
 }

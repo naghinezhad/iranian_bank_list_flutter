82aa6f24067ea80f797894308111289b24b1f8c8
Amir Ahadi <49079889+amirahadi@users.noreply.github.com>
2025-05-27 20:40:19 +0330

    verify IBAN tests added

--- test/iranian_banks_test.dart
+++ test/iranian_banks_test.dart
@@ -1,14 +1,22 @@
+import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:iranian_banks/iranian_banks.dart';
 
 void main() {
   group('BankService', () {
     test('returns correct bank info by card number', () {
-      const cardNumber = '62198619'; // test card number is for blu bank
+      const cardNumber =
+          '6037-9975-9912-3456'; // test card number is for blu bank
       final BankInfoView? bankInfo = IranianBanks.getBankFromCard(cardNumber);
 
       expect(bankInfo, isNotNull);
-      expect(bankInfo?.name, equals('blubank'));
+      expect(bankInfo?.name.runtimeType, String);
+      expect(bankInfo?.title.runtimeType, String);
+      expect(bankInfo?.logoPath.runtimeType, String);
+      expect(bankInfo?.primaryColor.runtimeType, Color);
+      expect(bankInfo?.lighterColor.runtimeType, Color);
+      expect(bankInfo?.darkerColor.runtimeType, Color);
+      expect(bankInfo?.secondaryColor.runtimeType, Color);
     });
 
     test('returns null for not find bank by cart number', () {
@@ -23,6 +31,13 @@ void main() {
       final bankInfo = IranianBanks.getBankFromIban(ibanNumber);
 
       expect(bankInfo, isNotNull);
+      expect(bankInfo?.name.runtimeType, String);
+      expect(bankInfo?.title.runtimeType, String);
+      expect(bankInfo?.logoPath.runtimeType, String);
+      expect(bankInfo?.primaryColor.runtimeType, Color);
+      expect(bankInfo?.lighterColor.runtimeType, Color);
+      expect(bankInfo?.darkerColor.runtimeType, Color);
+      expect(bankInfo?.secondaryColor.runtimeType, Color);
     });
 
     test('returns null for not find bank info by IBAN number', () {
@@ -34,16 +49,32 @@ void main() {
 
     test('returns true for valid card number', () {
       final cardNumber = '6280231348159681';
-      final bool validationCardNumber = IranianBanks.verifyCard(cardNumber);
+      final bool validationCardNumber =
+          IranianBanks.verifyCardNumber(cardNumber);
 
       expect(validationCardNumber, isTrue);
     });
 
     test('returns false for invalid card number', () {
       final cardNumber = '1111000000000000';
-      final bool validationCardNumber = IranianBanks.verifyCard(cardNumber);
+      final bool validationCardNumber =
+          IranianBanks.verifyCardNumber(cardNumber);
 
       expect(validationCardNumber, isFalse);
+    });
+
+    test('returns true for valid IBAN number', () {
+      final ibanNumber = 'IR850140040000310011296780';
+      final bool validationIbanNumber = IranianBanks.verifyIBAN(ibanNumber);
+
+      expect(validationIbanNumber, isTrue);
+    });
+
+    test('returns false for invalid IBAN number', () {
+      final ibanNumber = 'IR850540102600000000000000';
+      final bool validationIbanNumber = IranianBanks.verifyIBAN(ibanNumber);
+
+      expect(validationIbanNumber, isFalse);
     });
   });
 }
